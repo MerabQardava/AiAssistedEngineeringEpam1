4b75630b332462866e80cada03bd72c1d7d72850
Merab Kardava <merab.kardava.1@btu.edu.ge>
2025-08-06 04:45:56 +0400

    initial commit

---
+++ src/test/java/org/example/aiassistedengineeringepam1/AiAssistedEngineeringEpam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aiassistedengineeringepam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistedEngineeringEpam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
